6c77f606159f5b0a26dc8939b4e8e214ea8dddc9
machikodesu <jobhinam@gmail.com>
2023-08-08 15:40:29 +0900

    JSでレスポンスを処理する

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,9 +1,5 @@
-import { Application } from "@hotwired/stimulus"
+// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+import "@hotwired/turbo-rails"
+import "controllers"
+import "memo"
 
-const application = Application.start()
-
-// Configure Stimulus development experience
-application.debug = false
-window.Stimulus   = application
-
-export { application }
